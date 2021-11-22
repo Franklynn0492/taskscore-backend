d1aa3cd9e7f109bcf3113ceaba80fde399ca6da8
Your Name <you@example.com>
2021-11-23 00:21:41 +0100

    Added store endpoint

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use model::users::*;
 use repository::*;
 
 #[macro_use] extern crate rocket;
-extern crate dotenv;
 
 #[get("/")]
 fn hello() -> Json<String> {
@@ -19,12 +18,12 @@ fn hello() -> Json<String> {
 }
 
 #[get("/user/<id>")]
-fn get_user<'a>(id: u32, repository: &'a State<Repository>) -> Json<Option<&'a User>> {
+fn get_user<'a>(id: u32, repository: &'a State<Repository>) -> Json<Option<User>> {
     Json(repository.get_user(id))
 }
 
 #[get("/user/all")]
-fn get_all_users<'a>(repository: &'a State<Repository>) -> Json<&'a Vec<User>> {
+fn get_all_users<'a>(repository: &'a State<Repository>) -> Json<Vec<User>> {
     Json(repository.get_all_users())
 }
 
@@ -37,11 +36,11 @@ fn get_task<'a>(id: u32, repository: &'a State<Repository>) -> Json<Option<&'a T
 fn get_all_tasks<'a>(repository: &'a State<Repository>) -> Json<&'a Vec<Task>> {
     Json(repository.get_all_tasks())
 }
-/*
+
 #[post("/score/<user_id>/<task_id>")]
-fn score<'a>(user_id: u32, task_id: u32, repository: &'a mut State<Repository>) -> Json<Result<&'a User, String>> {
+fn score<'a>(user_id: u32, task_id: u32, repository: &'a State<Repository>) -> Json<Result<u16, String>> {
     Json(repository.score(user_id, task_id))
-}*/
+}
 
 #[catch(404)]
 fn not_found() -> Json<&'static str> {
@@ -55,7 +54,7 @@ async fn main() {
     let _ = rocket::build()
 
     .manage(Repository::init_repository())
-    .mount(context_root, routes![hello, get_user, get_all_users, get_task, get_all_tasks])
+    .mount(context_root, routes![hello, get_user, get_all_users, get_task, get_all_tasks, score])
     .register(context_root, catchers![not_found])
     .launch()
     .await;
--- src/repository.rs
+++ src/repository.rs
@@ -1,17 +1,16 @@
 use crate::model::{tasks::*, users::{*, self}};
+use std::sync::Mutex;
 
 pub struct Repository {
-    users: Box<Vec<User>>,
+    users: Box<Vec<Mutex<User>>>,
     tasks: Box<Vec<Task>>
 }
 
 impl Repository {
     pub fn init_repository() -> Repository {
-        let mut users = vec![
-            User::new(1, "Flori"),
-            User::new(2, "Michi"),
-            User::new(3, "Franki")
-        ];
+        let mut flori = User::new(1, "Flori");
+        let mut michi = User::new(2, "Michi");
+        let franki = User::new(3, "Franki");
 
         let tasks = vec![
             Task { id: 1, name: "Blumen gießen".to_owned(), points: 10, enabled: true},
@@ -20,27 +19,37 @@ impl Repository {
             Task { id: 4, name: "Kaffee kochen".to_owned(), points: 75, enabled: true},
         ];
 
-        users[0].score_task(tasks[0].clone());
-        users[0].score_task(tasks[0].clone());
-        users[0].score_task(tasks[1].clone());
-        users[0].score_task(tasks[0].clone());
-        users[0].score_task(tasks[3].clone());
-        users[0].score_task(tasks[2].clone());
+        flori.score_task(tasks[0].clone());
+        flori.score_task(tasks[0].clone());
+        flori.score_task(tasks[1].clone());
+        flori.score_task(tasks[0].clone());
+        flori.score_task(tasks[3].clone());
+        flori.score_task(tasks[2].clone());
         
-        users[1].score_task(tasks[0].clone());
-        users[1].score_task(tasks[1].clone());
-        users[1].score_task(tasks[2].clone());
-        users[1].score_task(tasks[3].clone());
+        michi.score_task(tasks[0].clone());
+        michi.score_task(tasks[1].clone());
+        michi.score_task(tasks[2].clone());
+        michi.score_task(tasks[3].clone());
+
+        let mut users = vec![
+            Mutex::new(flori),
+            Mutex::new(michi),
+            Mutex::new(franki)
+        ];
 
         Repository {users: Box::new(users), tasks: Box::new(tasks)}
     }
 
-    pub fn get_user<'a>(&'a self, id: u32) -> Option<&'a User> {
-        self.users.iter().find(|user| user.id == id)
+    pub fn get_user<'a>(&'a self, id: u32) -> Option<User> {
+        let userMutex = self.users.iter().find(|user| user.lock().unwrap().id == id);
+        match userMutex {
+            Some(userMut) => Some(userMut.lock().unwrap().clone()),
+            _ => None
+        }
     }
 
-    pub fn get_all_users<'a>(&'a self) -> &'a Vec<User> {
-        &self.users
+    pub fn get_all_users<'a>(&'a self) -> Vec<User> {
+        self.users.iter().map(|user_mutex| user_mutex.lock().unwrap().clone()).collect()
     }
 
     pub fn get_task<'a>(&'a self, id: u32) -> Option<&'a Task> {
@@ -51,15 +60,16 @@ impl Repository {
         &self.tasks
     }
 
-    pub fn score<'a>(&'a mut self, user_id: u32, task_id: u32) -> Result<&'a User, String> {
-        let user_opt = self.users.iter_mut().find(|user| user.id == user_id);
-        let user = user_opt.ok_or("User does not exist")?;
+    pub fn score<'a>(&'a self, user_id: u32, task_id: u32) -> Result<u16, String> {
+        let user_opt = self.users.iter().find(|user| user.lock().unwrap().id == user_id);
+        let user_mutex = user_opt.ok_or("User does not exist")?;
+        let mut user = user_mutex.lock().unwrap();
 
         let task_opt = self.tasks.iter().find(|task| task.id == task_id);
         let task = task_opt.ok_or("Task does not exist")?;
 
         user.score_task(task.clone());
 
-        Ok(user)
+        Ok(user.points)
     }
 }
