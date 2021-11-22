e8189ad91b86f5e6def6c2c65f7647705591d167
Your Name <you@example.com>
2021-11-22 03:37:53 +0100

    Added repository data

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,27 @@
 
 
 pub mod users {
+    use super::tasks::{self, Score, Task};
+
     #[derive(serde::Serialize)]
     pub struct User {
         pub id: u32,
         pub name: String,
+        pub points: u16,
+        pub scores: Vec<tasks::Score>,
+    }
+
+    impl User {
+        pub fn new(id: u32, name: &str) -> User {
+            User {id, name: name.to_owned(), points: 0, scores: vec![]}
+        }
+
+        pub fn score_task<'a>(& mut self, task: Task) {
+            self.points += task.points;
+
+            let score = Score::new(task);
+            self.scores.push(score);
+        }
     }
 }
 
@@ -12,6 +29,7 @@ pub mod tasks {
     use chrono::{DateTime, Utc};
 
     #[derive(serde::Serialize)]
+    #[derive(Clone)]
     pub struct Task {
         pub id: u32,
         pub name: String,
@@ -24,5 +42,12 @@ pub mod tasks {
         pub task: Task,
         pub points: u16,
         pub scored_at: DateTime::<Utc>,
+    }
+
+    impl Score {
+        pub fn new(task: Task) -> Score {
+            let points = task.points.clone();
+            Score { task, points, scored_at: chrono::Utc::now()}
+        }
     }
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,28 +1,52 @@
 use crate::model::{tasks::*, users::{*, self}};
 
 pub struct Repository {
-    users: Vec<User>,
-    tasks: Vec<Task>
+    users: Box<Vec<User>>,
+    tasks: Box<Vec<Task>>
 }
 
 impl Repository {
     pub fn init_repository() -> Repository {
-        let users = vec![
-            User { id: 1, name: "Flori".to_owned()},
-            User { id: 2, name: "Michi".to_owned()},
-            User { id: 3, name: "Franki".to_owned()}
+        let mut users = vec![
+            User::new(1, "Flori"),
+            User::new(2, "Michi"),
+            User::new(3, "Franki")
         ];
 
         let tasks = vec![
             Task { id: 1, name: "Blumen gießen".to_owned(), points: 10, enabled: true},
             Task { id: 2, name: "Stunden abgeben".to_owned(), points: 30, enabled: false},
+            Task { id: 2, name: "Spülmaschine ausräumen".to_owned(), points: 52, enabled: true},
             Task { id: 3, name: "Kaffee kochen".to_owned(), points: 75, enabled: true},
         ];
 
-        Repository {users, tasks}
+        users[0].score_task(tasks[0].clone());
+        users[0].score_task(tasks[0].clone());
+        users[0].score_task(tasks[1].clone());
+        users[0].score_task(tasks[0].clone());
+        users[0].score_task(tasks[3].clone());
+        users[0].score_task(tasks[2].clone());
+        
+        users[1].score_task(tasks[1].clone());
+        users[1].score_task(tasks[2].clone());
+        users[1].score_task(tasks[3].clone());
+
+        Repository {users: Box::new(users), tasks: Box::new(tasks)}
     }
 
     pub fn get_user<'a>(&'a self, id: u32) -> Option<&'a User> {
         self.users.iter().find(|user| user.id == id)
+    }
+
+    pub fn get_all_users<'a>(&'a self) -> &'a Vec<User> {
+        &self.users
+    }
+
+    pub fn get_task<'a>(&'a self, id: u32) -> Option<&'a Task> {
+        self.tasks.iter().find(|task| task.id == id)
+    }
+
+    pub fn get_all_tasks<'a>(&'a self) -> &'a Vec<Task> {
+        &self.tasks
     }
 }
