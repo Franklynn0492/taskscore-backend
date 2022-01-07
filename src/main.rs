83c1d44c581de26131e47a70cf85a7e326f294d2
Frank Langner <Frank.Langner@ausy-technologies.de>
2022-01-07 17:12:07 +0100

    Fixed responses for "not found" status codes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use model::{MessageResponder, Score, Session, User};
 use model::session::LoginRequest;
 use model::task::Task;
 use repository::Repository;
+use rocket::response::status::NotFound;
 use rocket::serde::json::Json;
 use rocket::State;
 use rocket::http::{Cookie, CookieJar};
@@ -20,8 +21,9 @@ fn hello() -> Json<String> {
 }
 
 #[get("/user/<id>")]
-fn get_user<'a>(id: u32, repository: &'a State<Repository>) -> Json<Option<User>> {
-    Json(repository.get_user(id))
+fn get_user<'a>(id: u32, repository: &'a State<Repository>) -> Option<Json<User>> {
+    // Option.map_or() returns a default value if the option is None, or otherwise applies a function to the in the Some(x) contained value, returning an option
+    repository.get_user(id).map_or(None, |user| Some(Json(user)))
 }
 
 #[get("/user")]
@@ -40,8 +42,8 @@ fn add_user<'a>(session: Session, user: User, repository: &'a State<Repository>)
 }
 
 #[get("/task/<id>")]
-fn get_task<'a>(id: u32, repository: &'a State<Repository>) -> Json<Option<Task>> {
-    Json(repository.get_task(id))
+fn get_task<'a>(id: u32, repository: &'a State<Repository>) -> Option<Json<Task>> {
+    repository.get_task(id).map_or(None, |task| Some(Json(task)))
 }
 
 #[get("/task/all")]
@@ -50,16 +52,20 @@ fn get_all_tasks<'a>(repository: &'a State<Repository>) -> Json<Vec<Task>> {
 }
 
 #[post("/score/<task_id>")]
-fn score<'a>(session: Session, task_id: u32, repository: &'a State<Repository>) -> Json<Result<u16, String>> {
+fn score<'a>(session: Session, task_id: u32, repository: &'a State<Repository>) -> Result<Json<u16>, NotFound<String>> {
     let user_mutex_guard = session.user.lock().unwrap();
     let user_id = user_mutex_guard.id;
     std::mem::drop(user_mutex_guard);
-    Json(repository.score(user_id, task_id))
+
+    match repository.score(user_id, task_id) {
+        Ok(new_score) => Ok(Json(new_score)),
+        Err(msg) => Err(NotFound(msg))
+    }
 }
 
 #[get("/score/<user_id>")]
-fn get_score_of_user<'a>(user_id: u32, repository: &'a State<Repository>) -> Json<Option<Vec<Score>>> {
-    Json(repository.get_user(user_id).and_then(|user| Some(user.scores)))
+fn get_score_of_user<'a>(user_id: u32, repository: &'a State<Repository>) -> Option<Json<Vec<Score>>> {
+    repository.get_user(user_id).and_then(|user| Some(Json(user.scores)))
 }
 
 #[get("/score")]
@@ -68,13 +74,16 @@ fn get_score_of_current_user<'a>(session: Session) -> Json<Vec<Score>> {
 }
 
 #[post("/session/login")]
-fn login<'a>(login_request: LoginRequest, repository: &'a State<Repository>, jar: &'a CookieJar<'_>) -> Json<Result<Session, String>> {
+fn login<'a>(login_request: LoginRequest, repository: &'a State<Repository>, jar: &'a CookieJar<'_>) -> Result<Json<Session>, NotFound<String>> {
     let session_result = repository.login(login_request);
-    if session_result.is_ok() {
-        let session_id: &str = session_result.as_ref().unwrap().id.as_str();
-        jar.add(Cookie::new("sid", session_id).into_owned());
+    match session_result {
+        Ok(session) => {
+            let session_id: &str = session.id.as_str();
+            jar.add(Cookie::new("sid", session_id).into_owned());
+            Ok(Json(session))
+        },
+        Err(error) => Err(NotFound(error))
     }
-    Json(session_result)
 }
 
 #[get("/session")]
@@ -83,13 +92,16 @@ fn get_current_session<'a>(session: Session) -> Json<Session> {
 }
 
 #[delete("/session/logout")]
-fn logout<'a>(session: Session, repository: &'a State<Repository>) -> Json<Result<(), String>> {
-    Json(repository.logout(&session.id))
+fn logout<'a>(session: Session, repository: &'a State<Repository>) -> Result<Json<()>, NotFound<String>> {
+    match repository.logout(&session.id) {
+        Ok(_) => Ok(Json(())),
+        Err(msg) => Err(NotFound(msg))
+    }
 }
 
 #[catch(404)]
-fn not_found() -> Json<&'static str> {
-    Json("Route not found")
+fn not_found() -> NotFound<()> {
+    NotFound(())
 }
 
 #[rocket::main]
