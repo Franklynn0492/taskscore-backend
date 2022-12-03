49a2da4ec91ec72701f86f78cda91db0bb2126bf
Frank Langner <Frank.Langner@ausy-technologies.de>
2022-12-03 05:03:46 +0100

    Demoted LegacyRepository

--- src/repository/neo4j_repsitory.rs
+++ src/repository/neo4j_repsitory.rs
@@ -7,11 +7,8 @@ use rocket::{tokio::{net::TcpStream, io::BufStream}, futures::lock::Mutex, http:
 use tokio_util::compat::*;
 use crate::{model::{User}, resource::http::responder::MessageResponder};
 
-use super::{legacy_repository::{LegacyRepository}, repository::Repository};
-
 pub struct Neo4JRepository {
     client: Mutex<Client<Compat<BufStream<TcpStream>>>>,
-    legacy_repo: LegacyRepository,  // TODO: Replace me
 }
 
 type ConnectionError = String;
@@ -19,65 +16,59 @@ impl Neo4JRepository {
     
 }
 
-#[async_trait]
-impl Repository for Neo4JRepository {
-    async fn get_user<'a>(&'a self, id: u32) -> Option<crate::model::User> {
-        self.legacy_repo.get_user(id).await
+impl Neo4JRepository {
+    pub async fn get_user<'a>(&'a self, id: u32) -> Option<crate::model::User> {
+        //self.legacy_repo.get_user(id).await
+        !unimplemented!();
     }
 
     // Todo: Remove (and rename find_user_by_username_const), is obsolete
-    async fn find_user_by_username<'a>(&'a self, username: &String) -> Option<std::sync::Arc<std::sync::Mutex<crate::model::User>>> {
-        self.legacy_repo.find_user_by_username(username).await
+    pub async fn find_user_by_username<'a>(&'a self, username: &String) -> Option<std::sync::Arc<std::sync::Mutex<crate::model::User>>> {
+        //self.legacy_repo.find_user_by_username(username).await
+        !unimplemented!();
     }
 
-    async fn find_user_by_username_const<'a>(&'a self, username: &String) -> Option<crate::model::User> {
-        let mut client = self.client.lock().await;
-
-        let statement = "MATCH (p:Person {username: $username}) RETURN p;";
-        let params = Params::from_iter(vec![("username", username.clone())]);
-
-        let records = Neo4JRepository::match_in_db(&mut client, statement, params).await?;
-
-        let node = Node::try_from(records[0].fields()[0].clone()).unwrap();
-
-        let user = User::from(node);
-
-        return Some(user)
+    pub async fn get_all_users<'a>(&'a self) -> Vec<crate::model::User> {
+        //self.legacy_repo.get_all_users().await
+        !unimplemented!();
     }
 
-    async fn get_all_users<'a>(&'a self) -> Vec<crate::model::User> {
-        self.legacy_repo.get_all_users().await
+    pub async fn get_task<'a>(&'a self, id: u32) -> Option<crate::model::Task> {
+        //self.legacy_repo.get_task(id).await
+        !unimplemented!();
     }
 
-    async fn get_task<'a>(&'a self, id: u32) -> Option<crate::model::Task> {
-        self.legacy_repo.get_task(id).await
+    pub async fn get_all_tasks<'a>(&'a self) -> Vec<crate::model::Task> {
+        //self.legacy_repo.get_all_tasks().await
+        !unimplemented!();
     }
 
-    async fn get_all_tasks<'a>(&'a self) -> Vec<crate::model::Task> {
-        self.legacy_repo.get_all_tasks().await
+    pub async fn get_session<'a>(&'a self, session_id: &String) -> Option<crate::model::Session> {
+        //self.legacy_repo.get_session(session_id).await
+        !unimplemented!();
     }
 
-    async fn get_session<'a>(&'a self, session_id: &String) -> Option<crate::model::Session> {
-        self.legacy_repo.get_session(session_id).await
+    pub async fn score<'a>(&'a self, user_id: u32, task_id: u32) -> Result<u16, String> {
+        //self.legacy_repo.score(user_id, task_id).await
+        !unimplemented!();
     }
 
-    async fn score<'a>(&'a self, user_id: u32, task_id: u32) -> Result<u16, String> {
-        self.legacy_repo.score(user_id, task_id).await
+    pub async fn create_and_add_user<'a>(&'a self, username: String, display_name: String, password: String, is_admin: bool) -> Result<std::sync::Arc<std::sync::Mutex<crate::model::User>>, String> {
+        //self.legacy_repo.create_and_add_user(username, display_name, password, is_admin).await
+        !unimplemented!();
     }
 
-    async fn create_and_add_user<'a>(&'a self, username: String, display_name: String, password: String, is_admin: bool) -> Result<std::sync::Arc<std::sync::Mutex<crate::model::User>>, String> {
-        self.legacy_repo.create_and_add_user(username, display_name, password, is_admin).await
+    pub async fn add_team<'a>(&'a self, team: crate::model::user::Team) -> Option<u32> {
+        //self.legacy_repo.add_team(team).await
+        !unimplemented!();
     }
 
-    async fn add_team<'a>(&'a self, team: crate::model::user::Team) -> Option<u32> {
-        self.legacy_repo.add_team(team).await
+    pub async fn add_user_to_team<'a>(&'a self, team_name: &String, user_id: u32, manager: crate::model::User) -> Result<(), String> {
+        //self.legacy_repo.add_user_to_team(team_name, user_id, manager).await
+        !unimplemented!();
     }
 
-    async fn add_user_to_team<'a>(&'a self, team_name: &String, user_id: u32, manager: crate::model::User) -> Result<(), String> {
-        self.legacy_repo.add_user_to_team(team_name, user_id, manager).await
-    }
-
-    async fn add_user<'a>(&'a self, _session: &crate::model::Session, user: crate::model::User) -> MessageResponder<u32> {
+    pub async fn add_user<'a>(&'a self, _session: &crate::model::Session, user: crate::model::User) -> MessageResponder<u32> {
         // Todo: Check if admin session
         // It is needed tochange the Mutex within it to the rocket version - and this is toom much work for me right now :)
 
@@ -110,16 +101,18 @@ impl Repository for Neo4JRepository {
             }
         }
 
-        Neo4JRepository::discard(&mut client).await;
+        //Neo4JRepository::discard(&mut client).await;
 
         result
     }
 
-    async fn login<'a>(&'a self, login_request: crate::model::session::LoginRequest) -> Result<crate::model::Session, String> {
-        self.legacy_repo.login(login_request).await
+    pub async fn login<'a>(&'a self, login_request: crate::model::session::LoginRequest) -> Result<crate::model::Session, String> {
+        //self.legacy_repo.login(login_request).await
+        !unimplemented!();
     }
 
-    async fn logout(&self, session_id: &String) -> Result<(), String> {
-        self.legacy_repo.logout(session_id).await
+    pub async fn logout(&self, session_id: &String) -> Result<(), String> {
+        //self.legacy_repo.logout(session_id).await
+        !unimplemented!();
     }
 }
