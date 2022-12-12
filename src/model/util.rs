5841f7d7388bd9e18ca7a04d7870b31461c4c3f5
Frank Langner <Frank.Langner@ausy-technologies.de>
2022-12-13 00:27:04 +0100

    added util::get_utc

--- src/model/util.rs
+++ src/model/util.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use bolt_client::bolt_proto::Value;
+use chrono::{DateTime, Utc, FixedOffset};
 
 
 pub fn get_string(properties: &HashMap<String, Value>, key: &str, alternative: &str) -> String  {
@@ -27,6 +28,19 @@ pub fn get_u16(properties: &HashMap<String, Value>, key: &str, alternative: u16)
 pub fn get_u32(properties: &HashMap<String, Value>, key: &str, alternative: u32) -> u32  {
     match properties.get(key) {
         Some(Value::Integer(val)) => *val as u32,
+        _ => alternative
+    }
+}
+
+pub fn get_utc(properties: &HashMap<String, Value>, key: &str, alternative: DateTime<Utc>) -> DateTime<Utc>  {
+    match properties.get(key) {
+        Some(Value::String(val)) => {
+            let fixed_offset_res = DateTime::<FixedOffset>::parse_from_rfc3339(val.as_str());
+            match fixed_offset_res {
+                Ok(fixed_offset) => DateTime::<Utc>::from(fixed_offset),
+                _ => alternative
+            }
+        },
         _ => alternative
     }
 }
