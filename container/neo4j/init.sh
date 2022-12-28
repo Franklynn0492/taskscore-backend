c73c0251f14e21e5c2d19fc5343c53466a6e3441
Frank Langner <Frank.Langner@ausy-technologies.de>
2022-12-28 14:01:30 +0100

    Updated neo4j init.sh

--- container/neo4j/init.sh
+++ container/neo4j/init.sh
@@ -2,8 +2,13 @@
 
 neo4j-admin dbms set-initial-password "${DB_PWD}"
 neo4j --verbose start
-sleep 15
-cypher-shell -u neo4j -p ${DB_PWD} < import/01_create.cypher
-cypher-shell -u neo4j -p ${DB_PWD} < import/02_data.cypher
+SEMAPHORE_FILE="/var/semaphore"
+
+if [ ! -f "$SEMAPHORE_FILE" ]; then
+    sleep 15
+    cypher-shell -u neo4j -p ${DB_PWD} < import/01_create.cypher
+    cypher-shell -u neo4j -p ${DB_PWD} < import/02_data.cypher
+    touch "$SEMAPHORE_FILE"
+fi
 
 tail -f /dev/null
